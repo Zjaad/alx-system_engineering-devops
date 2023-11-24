4e9e120e12567dbca47805ce83c08d420b417ceb
zjaad <zjaadani@gmail.com>
2023-11-24 15:51:08 -0800

    editing 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,9 @@
 #install Flask (2.1.0) from pip3
 package {'flask' :
-  ensure => '2.1.0',
+  ensure   => '2.1.0',
+  provider => 'pip3',
+}
+package { 'werkzeug':
+  ensure   => '0.16.1',
   provider => 'pip3',
 }
