d3e1198cea1cbeec2dfd2ebc5078c00a17d017d6
Zjaad <127573785+Zjaad@users.noreply.github.com>
2023-12-07 23:00:11 +0000

    Delete 0x0A-configuration_management directory

--- 0x0A-configuration_management/1-install_a_package.pp
+++
@@ -1,9 +0,0 @@
-#install Flask (2.1.0) from pip3
-package {'flask' :
-  ensure   => '2.1.0',
-  provider => 'pip3',
-}
-package { 'werkzeug':
-  ensure   => '0.16.1',
-  provider => 'pip3',
-}
--- 0x0A-configuration_management/2-execute_a_command.pp
+++
@@ -1,6 +0,0 @@
-# Execute command to kill killmenow
-exec { 'killmenow':
-  command  => 'pkill killmenow',
-  provider => 'shell',
-  returns  => [0, 1],
-}
