5b5833da943915f98cfc716311ecd534476c1b2d
Zjaad <127573785+Zjaad@users.noreply.github.com>
2023-12-07 22:59:30 +0000

    Delete 0x11-python-network_1 directory

--- 0x11-python-network_1/1-hbtn_header.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-"""Sends a request to the URL and displays the value of the X-Request-Id"""
-import urllib.request
-import sys
-if __name__ == "__main__":
-    link = sys.argv[1]
-    with urllib.request.urlopen(link) as r:
-        print(r.getheader('X-Request-Id'))
