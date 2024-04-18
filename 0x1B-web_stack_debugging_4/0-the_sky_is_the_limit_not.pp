271bcb7ceca4b80e1d648dd1f00bfb004d4bf09e
zjaad <zjaadani@gmail.com>
2024-04-18 19:22:06 +0000

    another commit

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -2,12 +2,10 @@
 # This manifest configures Nginx
 
 exec { 'increase_file_limit':
+  path    => '/bin',
   command  => 'sed -i "s/ULIMIT=\'-n 15\'/ULIMIT=\'-n 4096\'/" /etc/default/nginx',
-  provider => shell,
 }
 
 exec { 'restart_nginx':
   command  => 'service nginx restart',
-  provider => shell,
-  require  => Exec['increase_file_limit'],
 }
