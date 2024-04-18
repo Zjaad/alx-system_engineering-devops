c50c25f0b559b126fd92b30471252e4807856aff
zjaad <zjaadani@gmail.com>
2024-04-18 19:25:50 +0000

    another commit

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,11 +1,16 @@
 # 0-the_sky_is_the_limit_not.pp
 # This manifest configures Nginx
 
-exec { 'increase_file_limit':
-  path    => '/bin',
-  command  => 'sed -i "s/ULIMIT=\'-n 15\'/ULIMIT=\'-n 4096\'/" /etc/default/nginx',
+exec { 'increase_worker_connections':
+  command  => "sed -i 's/worker_connections 768;/worker_connections 2000;/' /etc/nginx/nginx.conf",
+  path     => '/bin:/usr/bin:/sbin:/usr/sbin',
+  unless   => "grep -q 'worker_connections 2000;' /etc/nginx/nginx.conf",
+  provider => shell,
+  notify   => Exec['restart_nginx'],
 }
 
 exec { 'restart_nginx':
-  command  => 'service nginx restart',
+  command     => 'service nginx restart',
+  provider    => shell,
+  refreshonly => true,
 }
